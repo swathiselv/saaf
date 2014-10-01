6d0c89964589b0edc889fa5d6116d4a3f255197e
sselvavi <sselvavi@akamai.com>
2014-09-30 23:37:49 -0400

    Updated navigation

---
+++ client/views/dashboard/gallery.js
@@ -0,0 +1,8 @@
+Template.gallery.rendered = function() {
+     $('#utopia-tiles li').wookmark({
+         container: $('#utopia-woomark'),
+         offset: 10,
+         itemWidth: 160,
+         autoResize: true
+     });
+};
---
+++ packages/packages.js
@@ -0,0 +1,4 @@
+Package.on_use(function (api) {
+  api.export("fullcalendar");
+  api.add_files("public/fullcalendar.js");
+});
